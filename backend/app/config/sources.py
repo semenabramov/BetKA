2b7249568ae5161b64ce1486cc1ab6872a8fe0b8
Semyon-Abramov <ab@mebel-vigo.ru>
2025-08-05 19:27:25 +0300

    Все ссылки на результаты

--- backend/app/config/sources.py
+++ backend/app/config/sources.py
@@ -2,15 +2,44 @@
 MATCH_RESULT_SOURCES = [
     {
         'name': 'Championat.com - Англия',
-        'url': 'https://www.championat.com/football/_england/tournament/6118/calendar/',
+        'url': 'https://www.championat.com/football/_germany/tournament/6646/calendar/',
+        'league': 'Германия — Вторая Бундеслига',
+        'parser': 'championat'
+    },
+    {
+        'name': 'Championat.com - Англия',
+        'url': 'https://www.championat.com/football/_germany/tournament/6644/calendar/',
+        'league': 'Германия — Бундеслига ',
+        'parser': 'championat'
+    },
+    {
+        'name': 'Championat.com - Англия',
+        'url': 'https://www.championat.com/football/_spain/tournament/6636/calendar/',
+        'league': 'Испания — Примера',
+        'parser': 'championat'
+    },
+    {
+        'name': 'Championat.com - Англия',
+        'url': 'https://www.championat.com/football/_italy/tournament/6548/calendar/',
+        'league': 'Италия — Серия А',
+        'parser': 'championat'
+    },
+    {
+        'name': 'Championat.com - Англия',
+        'url': 'https://www.championat.com/football/_france/tournament/6642/calendar/',
+        'league': 'Франция — Лига 1',
+        'parser': 'championat'
+    },
+    {
+        'name': 'Championat.com - Англия',
+        'url': 'https://www.championat.com/football/_england/tournament/6584/calendar/',
+        'league': 'Англия — Чемпионшип',    
+        'parser': 'championat'
+    },
+    {
+        'name': 'Championat.com - Англия',
+        'url': 'https://www.championat.com/football/_england/tournament/6592/calendar/',
         'league': 'Англия — Премьер-лига',
         'parser': 'championat'
     },
-    # В будущем можно добавить другие источники
-    # {
-    #     'name': 'Championat.com - Испания',
-    #     'url': 'https://www.championat.com/football/_spain/tournament/6119/calendar/',
-    #     'league': 'Испания — Ла Лига',
-    #     'parser': 'championat'
-    # }
 ] 
